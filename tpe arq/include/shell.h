14a820a2ea184e49064699fc9cb3e89daf0325ea
maxxts <maxxts@gmail.com>
2013-06-08 16:47:19 -0300

    Vuelvo a pushear el Shell

---
+++ tpe arq/include/shell.h
@@ -0,0 +1,17 @@
+
+
+#ifndef _shell_
+#define _shell_
+#define MAX_INPUT 75 
+
+
+void initializeShell();
+
+void printShell();
+
+void addToInput( char c );
+
+void checkInput();
+
+
+#endif
---
+++ tpe arq/src/shell.c
@@ -0,0 +1,116 @@
+
+
+/** INCLUDES **/
+
+
+#include "../include/kernel.h"
+#include "../include/kc.h"
+#include "../include/shell.h"
+
+
+
+
+/** Variables del Shell **/
+
+
+/* Arreglo que contiene los caracteres que se ingresaron en el shell.
+ * Esto permite que despues se pueda leer el texto ingresado y evita que el usuario borre caracteres que imprimio el sistema.
+ */
+char shellInput[MAX_INPUT + 1]; //Ultimo espacio reservado para cerrar la cadena en caso de llegar al tope.
+//Direccion actual del arreglo.
+char inputPos = 0; 
+
+
+
+void initializeShell() {
+
+		
+		/** BIENVENIDA **/
+
+		printShell();
+		
+		while(1)
+		{
+	
+			char inChar;
+			
+			//LEO EL BUFFER DEL TECLADO (GETC) Y GUARDO EN inChar
+			
+			if (inChar == '\t'){
+				
+				//Inhibo el TAB en la linea de comandos
+				
+				
+			}else if (inChar == '\b'){
+				
+				//Verifico la posicion. Si es 0 => No permito que borre.
+				if (inputPos > 0)
+				{
+					putc(inChar);
+					inputPos--;
+				}
+				
+				
+			}else if (inChar == '\n'){
+				
+				//Imprimo el ENTER
+				putc(inChar);
+				//ANALIZO COMANDOS
+				addToInput('\0'); //Cierro la cadena
+				checkInput();
+				//Vacio el buffer
+				inputPos = 0;
+				//Muestro el Shell
+				printShell();
+				
+			}else {
+			
+				//Agrego caracter al input
+				if (!MaxReached())
+				{
+					addToInput(inChar);
+					putc(inChar);
+				}else{
+				
+					//Alcanzo el final de la linea
+				
+				}
+				
+			
+			}
+			
+			
+					
+			
+			
+			
+
+		}
+}
+
+void addToInput( char c ){
+
+	shellInput[inputPos] = c;
+	inputPos++;
+
+}
+
+void checkInput(){
+
+	//Leo el INPUT y comparo con los comandos
+
+}
+
+
+void printShell(){
+
+	printf("user@OurOS:~$ ");
+}
+
+int MaxReached() {
+
+	return inputPos == MAX_INPUT - 1;
+	
+}
+
+
